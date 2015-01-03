fb9e8ae670e526aa328d4a83c7d7cc27fcd0b5fd
Corey J. Nolet <cjnolet@gmail.com>
2015-01-03 03:19:24 -0500

    Adding some more comments to classes

--- services/src/main/scala/cjn/support/SparkContextFactory.scala
+++ services/src/main/scala/cjn/support/SparkContextFactory.scala
@@ -2,19 +2,28 @@ package cjn.support
 
 import org.apache.spark.{SparkContext, SparkConf}
 
+/**
+ * A simple factory to create a SparkContext.
+ */
 object SparkContextFactory {
 
 
+  /**
+   * This method is responsible for shipping jars off to the executors and setting any base state on the
+   * SparkConf. It's important that we ship off our services jar so that the executors can access any
+   * classes we've created.
+   */
   def newSparkContext: SparkContext = {
 
-    val sparkAssemblyJar = sys.props.get("spark.assembly.jar").getOrElse("")
+    /**
+     * Just for simplicity, the spark home and the spark yarn uber-jar are coming in through system properties
+     */
     val sparkHome = sys.props.get("spark.home").getOrElse("")
 
     val sparkConf = new SparkConf()
-      .setJars(Array(SparkContext.jarOfClass(this.getClass).get, sparkAssemblyJar))
+      .setJars(Array(SparkContext.jarOfClass(this.getClass).get))
       .setSparkHome(sparkHome)
 
-
-    new SparkContext("yarn-client", "TestSparkWebApp", sparkConf)
+    new SparkContext("yarn-client", "TestSparkJettyServer", sparkConf)
   }
 }
--- war/src/main/java/cjn/controllers/TestController.java
+++ war/src/main/java/cjn/controllers/TestController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * A simple controller demonstrating the calling of a service method which will launch a spark job and return
+ * the results to the client. In a real web application, it would probably be best to use cometd or some means
+ * of returning the results asynchronously as the job times could outlive request timeouts.
+ */
 @Controller(value= "/test")
 public class TestController {
 
